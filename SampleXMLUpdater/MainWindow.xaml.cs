d6066ba7a077eeb385218861fc5b325e044985f2
Reginald Pentinio <reggiepen@gmail.com>
2025-02-02 16:11:53 +0800

    Updated implementation to be cancellable (added sleep to simulate long operation)

--- SampleXMLUpdater/MainWindow.xaml.cs
+++ SampleXMLUpdater/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
 using System.Xml;
+using System.Threading;
 
 namespace SampleXMLUpdater
 {
@@ -23,6 +24,7 @@ namespace SampleXMLUpdater
     public partial class MainWindow : Window
     {
         private string[] selectedFilePaths;
+        private CancellationTokenSource cts;
 
         public MainWindow()
         {
@@ -47,8 +49,10 @@ namespace SampleXMLUpdater
             }
         }
 
-        private void UpdateXML(string filePath)
+        private void UpdateXML(string filePath, System.Threading.CancellationToken token)
         {
+            token.ThrowIfCancellationRequested();
+
             try
             {
                 XmlDocument xmlDoc = new XmlDocument();
@@ -61,7 +65,9 @@ namespace SampleXMLUpdater
 
                 foreach (XmlNode oldNode in nodeList)
                 {
-                    // Create  new element called "disc"
+                    token.ThrowIfCancellationRequested();
+
+                    // Create new element called "disc"
                     XmlElement newNode = xmlDoc.CreateElement("disc");
 
                     // Copy children from the old node to the new node
@@ -78,6 +84,9 @@ namespace SampleXMLUpdater
                 string directoryPath = System.IO.Path.GetDirectoryName(filePath);
                 string fileNameNoExt = System.IO.Path.GetFileNameWithoutExtension(filePath);
                 xmlDoc.Save(System.IO.Path.Combine(directoryPath, fileNameNoExt + "_updated.xml"));
+
+                // Simulate a long running operation
+                Thread.Sleep(5000);
             }
             catch (Exception ex)
             {
@@ -85,14 +94,31 @@ namespace SampleXMLUpdater
             }
         }
 
-        private void UpdateXMLClick(object sender, RoutedEventArgs e)
+        private async void UpdateXMLClick(object sender, RoutedEventArgs e)
         {
-            foreach (string filePath in selectedFilePaths)
-            {
-                UpdateXML(filePath);
-            }
+            // Initialize the CancellationTokenSource.
+            cts = new CancellationTokenSource();
 
-            System.Windows.Forms.MessageBox.Show("Finished processing.");
+            var token = cts.Token;
+
+            try
+            {
+                foreach (string filePath in selectedFilePaths)
+                {
+                    await Task.Run(() => UpdateXML(filePath, token), token);
+                }
+
+                System.Windows.Forms.MessageBox.Show("Finished processing.");
+            }
+            catch (OperationCanceledException)
+            {
+                System.Windows.Forms.MessageBox.Show("Operation was canceled.");
+            }
+        }
+
+        private void CancelClick(object sender, RoutedEventArgs e)
+        {
+            cts.Cancel();
         }
     }
 }
